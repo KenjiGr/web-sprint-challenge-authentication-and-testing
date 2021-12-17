f6fad28f093c237a0c62811495b7d67957ae03a9
Kenji Greene <kenjigrr@gmail.com>
2021-12-17 11:33:18 -0600

    fixed token error messages

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -4,11 +4,11 @@ const { JWT_SECRET } = require('../secrets');
 module.exports = (req, res, next) => {
   const token = req.headers.authorization;
   if(!token){
-    return next({status: 4-1, message: 'token required'})
+    res.json({message: 'token required'})
   }
   jwt.verify(token, JWT_SECRET, (err, verified) => {
     if(err){
-      return next({status: 401, message: 'token required'})
+     res.json({message: 'token required'})
     }
     req.verifiedJwt = verified
     next()
