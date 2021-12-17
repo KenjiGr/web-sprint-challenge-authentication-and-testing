e105c48433a274c4355568e143f7351aebd69729
Kenji Greene <kenjigrr@gmail.com>
2021-12-17 11:21:21 -0600

    created restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,5 +1,18 @@
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../secrets');
+
 module.exports = (req, res, next) => {
-  next();
+  const token = req.headers.authorization;
+  if(!token){
+    return next({status: 4-1, message: 'token required'})
+  }
+  jwt.verify(token, JWT_SECRET, (err, verified) => {
+    if(err){
+      return next({status: 401, message: 'token required'})
+    }
+    req.verifiedJwt = verified
+    next()
+  })
   /*
     IMPLEMENT
 
